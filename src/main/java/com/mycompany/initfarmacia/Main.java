4223c70775bcf1bc2b3e89d8e0cc65940cf6bfd8
Gui <guilherme.ssoares13@hotmail.com>
2023-09-11 20:22:50 -0300

    Feat: Tela Login

---
+++ src/main/java/com/mycompany/initfarmacia/Main.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.initfarmacia;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        InitLogin login = new InitLogin();
+        login.setVisible(true);
+        
+    }
+}
