548d42452ec57646cc53b605f1d22a75880b5327
Ecthor Silva <EcthorSilva@outlook.com>
2023-10-03 17:29:42 -0300

    Fix: FlatLaf

--- src/main/java/controller/main.java
+++ src/main/java/controller/main.java
@@ -6,17 +6,17 @@ import view.TelaLogin;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Carregando...");
-        System.out.println("Pronto!");
-        
-        TelaLogin login = new TelaLogin(); // Instancia
-        login.setVisible(true);
-        
         // Muda o visual do Java Swing
         try {
             UIManager.setLookAndFeel(new FlatMacLightLaf());
         } catch (Exception ex) {
             System.err.println("Failed to initialize LaF");
         }
+        
+        System.out.println("Carregando...");
+        System.out.println("Pronto!");
+        
+        TelaLogin login = new TelaLogin(); // Instancia
+        login.setVisible(true);
     }
 }
--- src/main/java/view/TelaLogin.java
+++ src/main/java/view/TelaLogin.java
@@ -116,7 +116,7 @@ public class TelaLogin extends javax.swing.JFrame {
                 .addGroup(pnlLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(pswSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 146, Short.MAX_VALUE)
                 .addGroup(pnlLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnEntrar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
