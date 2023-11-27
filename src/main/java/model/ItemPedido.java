0d6348004d2d7c1329930e6839062f667583e4fa
Otavio Pereira <otavioaugustosilva.pe@gmail.com>
2023-11-26 23:07:53 -0300

    Branch Com javadoc criado

---
+++ src/main/java/model/ItemPedido.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Ectho
+ */
+public class ItemPedido {
+    private int idProduto;
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemPedido(int idProduto, int quantidade, double precoUnitario) {
+        this.idProduto = idProduto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
--- src/main/java/model/pedido.java
+++ src/main/java/model/pedido.java
@@ -1,12 +1,91 @@
 package model;
 
+import java.util.List;
+
 /**
  *
- * @author Ectho
+ * @author Ecthor
  */
 public class Pedido {
     private int idPedido;
-    private String nomeCliente;
-    private String nomeVendedor;
+    private int idCliente;
+    private int idVendedor;
+    private List<ItemPedido> itensPedido;
+    private String formaPagamento;
+    private double totalCompra;
     
+    // Construtor Vazio
+    public Pedido() {
+        
+    }
+    // Construtor sem o ID do Pedido
+    public Pedido(int idCliente, int idVendedor, List<ItemPedido> itensPedido, String formaPagamento, double totalCompra) {
+        this.idCliente = idCliente;
+        this.idVendedor = idVendedor;
+        this.itensPedido = itensPedido;
+        this.formaPagamento = formaPagamento;
+        this.totalCompra = totalCompra;
+    }
+    // Construtor com o ID do Pedido
+    public Pedido(int idPedido, int idCliente, int idVendedor, List<ItemPedido> itensPedido, String formaPagamento, double totalCompra) {
+        this.idPedido = idPedido;
+        this.idCliente = idCliente;
+        this.idVendedor = idVendedor;
+        this.itensPedido = itensPedido;
+        this.formaPagamento = formaPagamento;
+        this.totalCompra = totalCompra;
+    }
+    
+    public Pedido(int idPedido, int idCliente) {
+        this.idPedido = idPedido;
+        this.idCliente = idCliente;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public List<ItemPedido> getItensPedido() {
+        return itensPedido;
+    }
+
+    public void setItensPedido(List<ItemPedido> itensPedido) {
+        this.itensPedido = itensPedido;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public double getTotalCompra() {
+        return totalCompra;
+    }
+
+    public void setTotalCompra(double totalCompra) {
+        this.totalCompra = totalCompra;
+    }
 }
